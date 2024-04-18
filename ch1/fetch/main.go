a2bf80ca170d20689c5730b7ead164cd1b5c1bae
Shane Gibbons <shane.gibbons@gmail.com>
2024-04-17 21:39:56 -0500

    Ex 1.8 add missing url scheme if needed

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -6,10 +6,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !hasScheme(url) {
+			url = "https://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -22,4 +26,8 @@ func main() {
 			os.Exit(1)
 		}
 	}
+}
+
+func hasScheme(url string) bool {
+	return strings.HasPrefix(url, "https://") || strings.HasPrefix(url, "http://")
 }
